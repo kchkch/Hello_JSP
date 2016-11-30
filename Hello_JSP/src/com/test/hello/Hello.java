7d1d98c45f5571ab448fc23ed226e95312b20efa
KchKch <jyug13@naver.com>
2016-11-30 11:50:27 +0900

    modify>>Hello.java:012456 by Kim

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("local Repository create Hello_jsp");
+		//012456:hofix edit by Kim
+		System.out.println("hotfix edit/modify");
+		//012456:end
 	}
 
 }
