4dd3b6de806b228ef3bff382af7c0b3f0d758105
kchkch <jyug13@naver.com>
2016-12-01 10:09:17 +0900

    modify >> Hello.java:0278944 for pull by kim

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -15,6 +15,11 @@ public class Hello {
 		System.out.println("GitHub edit 4567");
 		
 		//0412518 end
+		
+		// 0278944:pull by kim
+		System.out.println("GitHub edit 4567891");
+		
+		// 0278944 end
 	}
 
 }
